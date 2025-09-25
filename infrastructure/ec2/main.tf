33a9fb9aec9f575d7d0a5c5642501be4e86ebaae
heathfrantz91-sys <heathfrantz91@gmail.com>
2025-09-25 17:26:38 -0400

    Update main.tf

--- infrastructure/ec2/main.tf
+++ infrastructure/ec2/main.tf
@@ -1,9 +1,8 @@
 module "ec2_instance" {
   source = "git::https://github.com/heathfrantz91-sys/Modules.git//aws/ec2?ref=main"
 
-  instance_name       = local.instance_name
+  name                = var.name
   instance_type       = var.instance_type
-  ami_id              = var.ami_id
   key_name            = var.key_name
   subnet_id           = var.subnet_id
   security_group_ids  = var.security_group_ids
