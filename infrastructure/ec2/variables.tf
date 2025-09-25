65ef111a0f03a309a5a44c7df0984504b2c63b3f
heathfrantz91-sys <heathfrantz91@gmail.com>
2025-09-25 17:31:15 -0400

    Create variables.tf

---
+++ infrastructure/ec2/variables.tf
@@ -0,0 +1,52 @@
+variable "create" {
+  description = "Whether to create an EC2 instance"
+  type        = bool
+  default     = true
+}
+
+variable "name" {
+  description = "Name of the EC2 instance"
+  type        = string
+  default     = "unkownec2instance"
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t3.micro"
+}
+
+variable "ami" {
+  description = "Optional AMI ID (if empty, latest Ubuntu will be used)"
+  type        = string
+  default     = ""
+}
+
+variable "key_name" {
+  description = "SSH key pair to attach"
+  type        = string
+  default     = null
+}
+
+variable "subnet_id" {
+  description = "Subnet ID where instance will be launched"
+  type        = string
+}
+
+variable "security_group_ids" {
+  description = "List of security group IDs"
+  type        = list(string)
+  default     = []
+}
+
+variable "instance_tags" {
+  description = "Additional tags for the instance"
+  type        = map(string)
+  default     = {}
+}
+
+variable "environment" {
+  description = "Environment (dev, qa, prod)"
+  type        = string
+  default     = "dev"
+}
