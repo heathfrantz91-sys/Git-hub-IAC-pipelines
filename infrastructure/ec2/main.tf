9f64f632224a1a126e913ff7f74798b3e4193234
Heath Frantz <heathfrantz@Mac-mini.local>
2025-09-25 16:28:27 -0400

    Update infrastructure EC2 module call to match custom module changes

--- infrastructure/ec2/main.tf
+++ infrastructure/ec2/main.tf
@@ -1,12 +1,12 @@
 module "ec2_instance" {
   source = "git::https://github.com/heathfrantz91-sys/Modules.git//aws/ec2?ref=main"
 
-  name          = local.instance_name
-  instance_type = var.instance_type
-  ami           = var.ami_id
-  key_name      = var.key_name
-  subnet_id     = var.subnet_id
-  vpc_security_group_ids = var.security_group_ids
-  instance_tags = local.final_tags
-  create        = true
+  instance_name       = local.instance_name
+  instance_type       = var.instance_type
+  ami_id              = var.ami_id
+  key_name            = var.key_name
+  subnet_id           = var.subnet_id
+  security_group_ids  = var.security_group_ids
+  instance_tags       = local.final_tags
+  create              = true
 }
