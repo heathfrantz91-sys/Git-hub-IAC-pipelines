9bc52ff533225d106477919881acb8b27e7b84ea
heathfrantz91-sys <heathfrantz91@gmail.com>
2025-09-29 11:37:03 -0400

    Update main.tf

--- infrastructure/ec2/main.tf
+++ infrastructure/ec2/main.tf
@@ -1,11 +1,20 @@
 module "ec2_instance" {
-  source = "git::https://github.com/heathfrantz91-sys/Modules.git//aws/ec2?ref=main"
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "~> 5.6.0"
 
   name                = var.name
   instance_type       = var.instance_type
   key_name            = var.key_name
   subnet_id           = var.subnet_id
   ami                 = var.ami
-  security_group_ids  = var.security_group_ids
-  create              = true
+  vpc_security_group_ids = var.security_group_ids
+  tags                = local.final_tags
+
+  dynamic "cpu_options" {
+    for_each = var.cpu_core_count != null && var.cpu_threads_per_core != null ? [1] : []
+    content {
+      core_count       = var.cpu_core_count
+      threads_per_core = var.cpu_threads_per_core
+    }
+  }
 }
