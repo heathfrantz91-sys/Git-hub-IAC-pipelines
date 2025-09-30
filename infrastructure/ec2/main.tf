363547fd2dc34afb2be173139696beab36149734
Heath Frantz <heathfrantz91@gmail.com>
2025-09-30 12:46:24 -0400

    fixed stupid mistake on the source url

--- infrastructure/ec2/main.tf
+++ infrastructure/ec2/main.tf
@@ -33,8 +33,8 @@ locals {
 
 # EC2 instance using your custom GitHub module
 module "ec2_instance" {
-  source = "git::https://github.com/heathfrantz91-sys/Git-hub-IAC-pipelines"
-
+  source = "git::https://github.com/heathfrantz91-sys/Modules.git//aws/ec2?ref=main"
+  
   name                  = var.name
   instance_type         = var.instance_type
   key_name              = var.key_name
